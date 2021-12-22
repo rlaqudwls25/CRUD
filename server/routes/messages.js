82d19ab91679d2e4895f00dbeb221bd353bd72f5
rlaqudwls25 <rlaqudwls157@gmail.com>
2021-12-23 00:02:44 +0900

    server Routes 작업 중

---
+++ server/routes/messages.js
@@ -0,0 +1,10 @@
+const messagesRoute = [
+    {
+        //전체 메시지를 가져오는 역활
+        method: 'get',
+        route: '/messages',
+        handler: (req, res) => {
+            res.send()
+        }
+    }
+]
