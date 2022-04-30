0839eca50f7090c98349a10d0210b175a3fea90b
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-04-30 18:23:02 +0900

    fetcher(axios) 함수 생성

---
+++ client/fetcher.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8000';
+
+const fetcher = async (method, url, ...rest) => {
+  const res = await axios[method](url, ...rest);
+  return res.data;
+};
